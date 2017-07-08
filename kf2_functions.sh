60df619c952ac6ca82022e5141fa16941b4f913a
Mike Cronce <mike@quadra-tec.net>
2017-07-08 17:13:47 -0400

    kf2_functions.sh:  Added support for "$KF_PORT", "$KF_QUERY_PORT", and "$KF_WEBADMIN_PORT" environment variables

--- kf2_functions.sh
+++ kf2_functions.sh
@@ -71,6 +71,16 @@ function load_config() {
     # true or false, default false
     [[ -z "$KF_ENABLE_WEB" ]] && export KF_ENABLE_WEB=false
 
+    # default to 7777
+    [[ -z "$KF_PORT" ]] && export KF_PORT=7777
+
+    # default to $(($KF_PORT + 19238))
+    #    (19238 = 27015 - 7777)
+    [[ -z "$KF_QUERY_PORT" ]] && export KF_QUERY_PORT="$(($KF_PORT + 19238))"
+
+    # default to 8080
+    [[ -z "$KF_WEBADMIN_PORT" ]] && export KF_WEBADMIN_PORT=7777
+
 
     ## Now we edit the config files to set the config
     sed -i "s/^GameLength=.*/GameLength=$KF_GAME_LENGTH\r/" "${HOME}/kf2server/KFGame/Config/LinuxServer-KFGame.ini"
@@ -87,9 +97,9 @@ function launch() {
     cmd+="?Difficulty=$KF_DIFFICULTY"
     cmd+="?AdminPassword=$KF_ADMIN_PASS"
     [[ -z "$KF_GAME_PASS" ]] || cmd+="?GamePassword=$KF_GAME_PASS"
-    cmd+=" -Port=7777"
-    cmd+=" -WebAdminPort=8080"
-    cmd+=" -QueryPort=27015"
+    cmd+=" -Port=$KF_PORT"
+    cmd+=" -WebAdminPort=$KF_WEBADMIN_PORT"
+    cmd+=" -QueryPort=$KF_QUERY_PORT"
 
     echo "Running command: $cmd" > $0-cmd.log
     exec $cmd
